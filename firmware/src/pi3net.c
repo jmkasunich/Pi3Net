52d07bc84e69e9f4bf486b90b035411d7ef8dcb8
John Kasunich <jmkasunich@fastmail.fm>
2025-07-05 17:56:24 -0400

    work in progress; capturing a working snapshot

--- firmware/src/pi3net.c
+++ firmware/src/pi3net.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
 #include "pi3net.h"
 #include "pi3net.pio.h"
@@ -8,23 +9,49 @@
 
 
 
-const p3n_hwdef_t p3n_bus1 = { 1, 1, 1, 16, 16, 17 };
-const p3n_hwdef_t p3n_bus2 = { 1, 1, 1, 16, 16, 28 };
-const p3n_hwdef_t p3n_tx_up = { 1, 0, 0, 4, 0, 0 };
-const p3n_hwdef_t p3n_rx_up = { 0, 1, 0, 0, 5, 0 };
-const p3n_hwdef_t p3n_tx_dn = { 1, 0, 0, 2, 0, 0 };
-const p3n_hwdef_t p3n_rx_dn = { 0, 1, 0, 0, 2, 0 };
+const p3n_link_hw_t p3n_bus1  = { 1, 1, 1, 16, 16, 17 };
+const p3n_link_hw_t p3n_bus2  = { 1, 1, 1, 22, 22, 28 };
+const p3n_link_hw_t p3n_tx_up = { 1, 0, 0, 4, 0, 0 };
+const p3n_link_hw_t p3n_rx_up = { 0, 1, 0, 0, 5, 0 };
+const p3n_link_hw_t p3n_tx_dn = { 1, 0, 0, 2, 0, 0 };
+const p3n_link_hw_t p3n_rx_dn = { 0, 1, 0, 0, 3, 0 };
 
 
 #define SCOPE_CHAN_1  (10)
 #define SCOPE_CHAN_2  (14)
 #define SCOPE_CHAN_3  (6)
+#define SCOPE_CHAN_4  (8)
+
+#define NUM_BUF 4
+
+p3n_buffer_t buffers[NUM_BUF];
+
+p3n_buffer_t *new_buffer(uint max_size)
+{
+    p3n_buffer_t *b;
+    b = malloc(sizeof(p3n_buffer_t));
+    if ( b == NULL ) {
+        return NULL;
+    }
+    b->data = malloc(max_size);
+    if ( b->data == NULL ) {
+        free(b);
+        return NULL;
+    }
+    b->max_len = max_size;
+    b->data_len = 0;
+    return b;
+}
 
 
-static inline void pi3net_tx_program_init(PIO pio, uint sm, uint offset, uint tx_pin, uint rx_pin) {
-    pio_sm_config c = pi3net_program_get_default_config(offset);
+p3n_buffer_t *buf1 = NULL;
+p3n_buffer_t *buf2 = NULL;
 
-    // Map the state machine's OUT pin group to one pin, namely the `tx_pin`
+
+static inline void p3n_tx_program_init(PIO pio, uint sm, uint offset, uint tx_pin, uint side_pin) {
+    pio_sm_config c = p3n_tx_program_get_default_config(offset);
+
+    // Map the state machine's OUT and SET pin groups to one pin, namely the `tx_pin`
     // parameter to this function.
     sm_config_set_out_pins(&c, tx_pin, 1);
     sm_config_set_set_pins(&c, tx_pin, 1);
@@ -33,14 +60,12 @@ static inline void pi3net_tx_program_init(PIO pio, uint sm, uint offset, uint tx
     // Set the pin direction to output at the PIO
     pio_sm_set_consecutive_pindirs(pio, sm, tx_pin, 1, true);
 
-    // Map the state machine's IN pin group to one pin, namely the `rx_pin`
-    // parameter to this function.
-    sm_config_set_in_pins(&c, rx_pin);
-
-    sm_config_set_out_shift(&c, true, false, 8);
-    sm_config_set_mov_status(&c, 0, 1);
-
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_TX);
+    // map the sideset pin (this is temporary for debug only)
+    sm_config_set_sideset_pins(&c, side_pin);
+    // Set this pin's GPIO function (connect PIO to the pad)
+    pio_gpio_init(pio, side_pin);
+    // Set the pin direction to output at the PIO
+    pio_sm_set_consecutive_pindirs(pio, sm, side_pin, 1, true);
 
     // Load our configuration, and jump to the start of the program
     pio_sm_init(pio, sm, offset, &c);
@@ -48,112 +73,154 @@ static inline void pi3net_tx_program_init(PIO pio, uint sm, uint offset, uint tx
     pio_sm_set_enabled(pio, sm, true);
 }
 
-static inline void pi3net_rx_program_init(PIO pio, uint sm, uint offset, uint tx_pin, uint rx_pin) {
-    pio_sm_config c = pi3net_program_get_default_config(offset);
-
-    // Map the state machine's OUT pin group to one pin, namely the `tx_pin`
-    // parameter to this function.
-    sm_config_set_out_pins(&c, tx_pin, 1);
-    sm_config_set_set_pins(&c, tx_pin, 1);
-    // Set this pin's GPIO function (connect PIO to the pad)
-    pio_gpio_init(pio, tx_pin);
-    // Set the pin direction to output at the PIO
-    pio_sm_set_consecutive_pindirs(pio, sm, tx_pin, 1, true);
+static inline void p3n_rx_program_init(PIO pio, uint sm, uint offset, uint rx_pin, uint side_pin) {
+    pio_sm_config c = p3n_rx_program_get_default_config(offset);
 
     // Map the state machine's IN pin group to one pin, namely the `rx_pin`
     // parameter to this function.
     sm_config_set_in_pins(&c, rx_pin);
     sm_config_set_jmp_pin(&c, rx_pin);
 
-    sm_config_set_out_shift(&c, true, false, 8);
-    sm_config_set_mov_status(&c, 0, 1);
-
-    sm_config_set_fifo_join(&c, PIO_FIFO_JOIN_RX);
+    // map the sideset pin (this is temporary for debug only)
+    sm_config_set_sideset_pins(&c, side_pin);
+    // Set this pin's GPIO function (connect PIO to the pad)
+    pio_gpio_init(pio, side_pin);
+    // Set the pin direction to output at the PIO
+    pio_sm_set_consecutive_pindirs(pio, sm, side_pin, 1, true);
 
     // Load our configuration, and jump to the start of the program
-    pio_sm_init(pio, sm, (offset + pi3net_offset_rxbegin) & 0x1F, &c);
+    pio_sm_init(pio, sm, (offset + p3n_rx_offset_rxbegin) & 0x1F, &c);
     // Set the state machine running
     pio_sm_set_enabled(pio, sm, true);
 }
 
-#define P3N_NUM_CHANS 2
+#define P3N_MAX_SM 4
 
-static_assert(P3N_NUM_CHANS > 0 );
-static_assert(P3N_NUM_CHANS <= 4 );
-
-PIO p3n_pio;
-uint p3n_sms[P3N_NUM_CHANS];
-uint p3n_code_offset;
+PIO p3n_pio = NULL;
+uint p3n_sms[P3N_MAX_SM] = { -1, -1, -1, -1 };
+uint p3n_tx_code_offset = -1;
+uint p3n_rx_code_offset = -1;
 
 
-bool is_pio_available(PIO pio)
+bool p3n_try_pio(PIO pio, uint num_sm)
 {
-
-    // will the program fit?
-    if ( ! pio_can_add_program(pio, &pi3net_program) ) {
-        return false;
+    // attempt to load first program
+    p3n_tx_code_offset = pio_add_program(pio, &p3n_tx_program);
+    if ( p3n_tx_code_offset < 0 ) {
+        goto error;
     }
-    // are there enough free state machines?
-    uint sm_free = 0;
-    for ( int n = 0 ; n < 4 ; n++ ) {
-        if ( ! pio_sm_is_claimed(pio, n) ) {
-            sm_free++;
+    // attempt to load second program
+    p3n_rx_code_offset = pio_add_program(pio, &p3n_rx_program);
+    if ( p3n_rx_code_offset < 0 ) {
+        goto error;
+    }
+    // programs loaded, claim the state machines
+    for ( int n = 0 ; n < num_sm ; n++ ) {
+        p3n_sms[n] = pio_claim_unused_sm(pio, false);
+        if ( p3n_sms[n] < 0 ) {
+            goto error;
         }
     }
-    if ( sm_free < P3N_NUM_CHANS ) {
-        return false;
-    }
+    p3n_pio = pio;
     return true;
+
+    // error handling; free resources in the reverse order of obtaining them
+    error:
+    // unclaim any successfully allocated state machines
+    for ( int n = 0 ; n < num_sm ; n++ ) {
+        if ( p3n_sms[n] >= 0 ) {
+            pio_sm_unclaim(pio, p3n_sms[n] );
+            p3n_sms[n] = -1;
+        }
+    }
+    // unload any successfully loaded programs
+    if ( p3n_rx_code_offset >= 0 ) {
+        pio_remove_program(pio, &p3n_rx_program, p3n_rx_code_offset);
+        p3n_rx_code_offset = -1;
+    }
+    if ( p3n_tx_code_offset >= 0 ) {
+        pio_remove_program(pio, &p3n_tx_program, p3n_tx_code_offset);
+        p3n_tx_code_offset = -1;
+    }
+    p3n_pio = NULL;
+    return false;
 }
 
-
-
-bool load_program_claim_sms(void)
+bool load_programs_claim_sms(uint num_sm)
 {
-    if ( is_pio_available(pio0) ) {
-        p3n_pio = pio0;
-    } else if ( is_pio_available(pio1) ) {
-        p3n_pio = pio1;
-    } else return false;
-    // load the program
-    p3n_code_offset = pio_add_program(p3n_pio, &pi3net_program);
-    hard_assert(p3n_code_offset >= 0 );
-    // claim the state machines
-    for ( int n = 0 ; n < P3N_NUM_CHANS ; n++ ) {
-        p3n_sms[n] = pio_claim_unused_sm(p3n_pio, 1);
+    if ( ! p3n_try_pio(pio0, num_sm) ) {
+        if ( ! p3n_try_pio(pio1, num_sm) ) {
+            return false;
+        }
     }
-
     printf("Using PIO # %d, state machines ", pio_get_index(p3n_pio));
-    for ( int n = 0 ; n < P3N_NUM_CHANS ; n++ ) {
+    for ( int n = 0 ; n < P3N_MAX_SM ; n++ ) {
         printf(" %d", p3n_sms[n]);
     }
-    printf("\n");
+    printf("\nTX code: %d\nRX code: %d\n", p3n_tx_code_offset, p3n_rx_code_offset);
+}
+
+uint32_t irq_count = 0;
+
+void p3n_pio_irq_handler(void)
+{
+    irq_count++;
+    pio_interrupt_clear(p3n_pio, 0);
+    printf("i");
+}
+
+void print_irq_globals(uint stamp)
+{
+    printf("%d: IRQ: %08x  INTR: %08x  RXF: %d CNT: %10d\n", stamp,  p3n_pio->irq, p3n_pio->intr, 
+                                pio_sm_get_rx_fifo_level(p3n_pio, p3n_sms[1]), irq_count);
 }
 
 void p3n_test(void)
 {
+    uint old_count;
+
     printf("Hello from pi3net\n");
+    buf1 = new_buffer(100);
+    assert(buf1);
+    buf2 = new_buffer(500);
+    assert(buf2);
+
     gpio_init(SCOPE_CHAN_1);
     gpio_set_dir(SCOPE_CHAN_1, GPIO_OUT);
-    load_program_claim_sms();
-    pi3net_tx_program_init(p3n_pio, p3n_sms[0], p3n_code_offset, SCOPE_CHAN_2, SCOPE_CHAN_3);
-    pio_sm_set_clkdiv(p3n_pio, p3n_sms[0], 15.625);
-    pi3net_rx_program_init(p3n_pio, p3n_sms[1], p3n_code_offset, SCOPE_CHAN_3, SCOPE_CHAN_2);
-    pio_sm_set_clkdiv(p3n_pio, p3n_sms[1], 15.625);
+    load_programs_claim_sms(3);
+    p3n_tx_program_init(p3n_pio, p3n_sms[0], p3n_tx_code_offset, SCOPE_CHAN_2, SCOPE_CHAN_4);
+    pio_sm_set_clkdiv(p3n_pio, p3n_sms[0], 15625.0);
+    p3n_rx_program_init(p3n_pio, p3n_sms[1], p3n_rx_code_offset, SCOPE_CHAN_2, SCOPE_CHAN_3);
+    pio_sm_set_clkdiv(p3n_pio, p3n_sms[1], 15625.0);
+    irq_add_shared_handler(PIO0_IRQ_0, p3n_pio_irq_handler, PICO_SHARED_IRQ_HANDLER_DEFAULT_ORDER_PRIORITY);
+    irq_set_enabled(PIO0_IRQ_0, true);
+    pio_set_irq0_source_enabled(p3n_pio, pis_interrupt0, true);
     printf("init done\n");
     while(1) {
-        sleep_ms(2);
+        old_count = irq_count;
+        print_irq_globals(1);
+        //pio_interrupt_clear(p3n_pio, 0);
         gpio_put(SCOPE_CHAN_1, 1);
         pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0xF0F0F0F0);
         pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0x550FF0AA);
-        pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0xDEADBE00);
+//        pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0xDEADBE00);
         pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0xDEADBEFF);
         pio_sm_put_blocking(p3n_pio, p3n_sms[0], 0xDEADBE3C);
+        while ( irq_count == old_count ) {
+            print_irq_globals(2);
+        }
+        print_irq_globals(3);
+        print_irq_globals(4);
+        print_irq_globals(5);
+        print_irq_globals(6);
         printf("\nfifo: %d  ", pio_sm_get_rx_fifo_level(p3n_pio, p3n_sms[1]));
         while ( pio_sm_get_rx_fifo_level(p3n_pio, p3n_sms[1]) > 0 ) {
             printf("%08x ", pio_sm_get(p3n_pio, p3n_sms[1]));
         }
-        sleep_ms(2);
+        printf("\n");
+        print_irq_globals(7);
+        sleep_ms(5000);
+        print_irq_globals(8);
         gpio_put(SCOPE_CHAN_1, 0);
     }
 
--- firmware/src/pi3net.h
+++ firmware/src/pi3net.h
@@ -10,24 +10,64 @@
 #define PI3NET_H
 
 #include <stdint.h>    // int32_t, uint32_t
+#include "hardware/structs/pio.h"
 
+
+
+/**********************************************************
+ * This structure defines a link in terms of the available 
+ * hardware; is it transmit only, recieve only, or both?
+ * Does it use a pin to enable an external line driver?
+ * Typically there will be one of these structures in flash
+ * for each hardware link; when calling the API functions
+ * you pass a pointer to the p3n_link_hw_t structure for
+ * the link you want to talk or listen on.
+ */
 #define PIN_ID_BITS  5
 
-typedef struct p3n_hwdef_s {
+typedef struct p3n_link_hw_s {
     uint32_t can_transmit       : 1;
     uint32_t can_recieve        : 1;
     uint32_t has_tx_ena         : 1;
     uint32_t transmit_pin       : PIN_ID_BITS;
     uint32_t receive_pin        : PIN_ID_BITS;
     uint32_t tx_ena_pin         : PIN_ID_BITS;
-} p3n_hwdef_t;
+} p3n_link_hw_t;
 
-extern const p3n_hwdef_t p3n_bus1;
-extern const p3n_hwdef_t p3n_bus2;
-extern const p3n_hwdef_t p3n_tx_up;
-extern const p3n_hwdef_t p3n_rx_up;
-extern const p3n_hwdef_t p3n_tx_dn;
-extern const p3n_hwdef_t p3n_rx_dn;
+/**********************************************************
+ * This structure defines a buffer for data transfer 
+ */
+
+ typedef struct p3n_buffer_s {
+    uint16_t    max_len;
+    uint16_t    data_len;
+    uint32_t    *data;
+ } p3n_buffer_t;
+
+
+
+typedef enum {
+    P3N_LINK_IDLE = 0,
+    P3N_LINK_SEND,
+    P3N_LINK_LISTEN
+} p3n_link_status_t;
+
+
+/**********************************************************
+ * This structure defines the status of a hardware link.
+ * Since the link status is dynamic, these are stored in 
+ * RAM.
+ */
+typedef struct p3n_link_s {
+    p3n_link_status_t   status;
+    uint32_t            sm_index;
+    p3n_link_hw_t       *hardware;
+    p3n_buffer_t        *buffer;
+
+} p3n_link_t;
+
+
+
 
 void p3n_test(void);
 
